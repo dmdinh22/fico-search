c75c7ccf71f90edf56affb4f11aa6941e2472418
David Dinh <dmdinh22@gmail.com>
2020-02-22 00:37:12 -0800

    scan file and simple timing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## FiCo Search
 - Processing streams with GoLang
+
+### Running Locally
+- Be in project root dir
+- Install Deps: `go install`
+- Build: `go build`
+- Run: `go run app.go`
--- app.go
+++ app.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	start := time.Now()
+	timeout := 60 * 1000 * time.Millisecond
+
+	fileInfo, err := os.Stat("./smallSample.txt")
+	checkForError(err)
+	fmt.Printf("the file is %d bytes long", fileInfo.Size())
+
+	fileToScan, err := ioutil.ReadFile("./smallSample.txt")
+	checkForError(err)
+	writer := bufio.NewWriter(os.Stdout)
+	buffedFile := bytes.NewReader(fileToScan)
+
+	scannedFile := bufio.NewScanner(buffedFile)
+	scannedFile.Split(bufio.ScanWords)
+
+	for scannedFile.Scan() {
+		elapsed := time.Since(start)
+
+		if elapsed > timeout {
+			fmt.Println("the process has timed out - elapsed:", elapsed)
+			log.Fatal("timed out...")
+		}
+
+		word := scannedFile.Bytes()
+		wordAsString := strings.ToLower(string(word))
+		if strings.Contains(wordAsString, "fico") {
+			fmt.Println(wordAsString)
+		}
+	}
+	if err := scannedFile.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	writer.Flush()
+
+	end := time.Now()
+	log.Println("finished", end.Sub(start))
+}
+
+func checkForError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
